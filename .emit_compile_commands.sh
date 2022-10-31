6fd033a5fd232cea0ea7a03df4f9dc637cfb18e8
Jorri Fransen <jorri@jorrifransen.nl>
2022-10-31 16:42:08 +0000

    Added script to emit compile_commands.json (for integration with nvim/coc/coc-clangd).

---
+++ .emit_compile_commands.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+. clean_all.sh
+exec bear -- ./build_all.sh
--- zodiac_driver/src/main.cpp
+++ zodiac_driver/src/main.cpp
@@ -13,13 +13,13 @@ int main() {
     auto ca = c_allocator();
 
     llvm_test();
-    
+
     Atom_Table at;
     atom_table_init(c_allocator(), &at);
-    
+
     auto match = atom_get(&at, "a") == atom_get(&at, "a");
-        
+
     atom_table_free(&at);
-    
+
     return 0;
-}   
+}
