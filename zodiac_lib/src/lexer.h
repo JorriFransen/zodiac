da32f5ceffa9e024ce5883e83687228ddba9f317
Jorri Fransen <jorri@jorrifransen.nl>
2022-11-30 17:38:36 +0000

    Removed token.h, moved token structure into lexer for now.

--- zodiac_lib/src/lexer.h
+++ zodiac_lib/src/lexer.h
@@ -2,12 +2,43 @@
 
 #include <defines.h>
 
-#include <token.h>
 #include <zodiac_context.h>
 #include <zstring.h>
 
 namespace Zodiac
 {
+
+enum Token_Kind
+{
+    TOK_INVALID = 0,
+
+    // Ascii range
+
+    TOK_INT = 128,
+    TOK_REAL,
+    TOK_NAME,
+    TOK_KEYWORD,
+
+    TOK_EQ,
+    TOK_NEQ,
+    TOK_LTEQ,
+    TOK_GTEQ,
+
+    TOK_EOF,
+};
+
+struct Token
+{
+    Token_Kind kind;
+
+    Atom atom;
+
+    union
+    {
+        u64 integer;
+        Real_Value real;
+    };
+};
 
 struct Lexer
 {
