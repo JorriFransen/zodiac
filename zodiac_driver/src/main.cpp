a0e8b192f5b474d750b37148eccf61a41bb4bb4f
Jorri Fransen <jorri@jorrifransen.nl>
2022-12-21 23:43:09 +0100

    Flattening the test code.

--- zodiac_driver/src/main.cpp
+++ zodiac_driver/src/main.cpp
@@ -1,17 +1,18 @@
 #include "asserts.h"
+#include "ast.h"
+#include "containers/dynamic_array.h"
+#include "defines.h"
 #include "lexer.h"
 #include "logger.h"
 #include "memory/allocator.h"
 #include "memory/zmemory.h"
 #include "parser.h"
 #include "platform/filesystem.h"
-#include "resolve.h"
+// #include "resolve.h"
 #include "zodiac_context.h"
 #include "zstring.h"
 
 using namespace Zodiac;
-
-namespace Zodiac { struct AST_File; }
 
 void flat_resolve_test(AST_File *file);
 
@@ -24,7 +25,7 @@ int main() {
     zodiac_context_create(&c);
 
     // TODO: CLEANUP: Used in the resolver
-    ctx = &c;
+    // ctx = &c;
 
     Lexer lexer;
     lexer_create(&c, &lexer);
@@ -59,6 +60,8 @@ enum class Flat_Node_Kind
     STMT,
     EXPR,
     TS,
+
+    PARAM_DECL,
 };
 
 struct Flat_Node
@@ -71,6 +74,8 @@ struct Flat_Node
         AST_Statement *stmt;
         AST_Expression *expr;
         AST_Type_Spec *ts;
+
+        AST_Field_Declaration param;
     };
 };
 
@@ -92,6 +97,7 @@ Flat_Node to_flat_node(AST_Declaration *decl);
 Flat_Node to_flat_node(AST_Statement *stmt);
 Flat_Node to_flat_node(AST_Expression *expr);
 Flat_Node to_flat_node(AST_Type_Spec *ts);
+Flat_Node to_flat_node(const AST_Field_Declaration param);
 
 void flat_resolve_test(AST_File *file)
 {
@@ -100,14 +106,19 @@ void flat_resolve_test(AST_File *file)
 
     for (u64 i = 0; i < file->declarations.count; i++) {
 
-        Flat_Root_Node node;
+        Flat_Root_Node node = {};
         node.root.kind = Flat_Node_Kind::DECL;
         node.root.decl = file->declarations[i];
 
         dynamic_array_create(&dynamic_allocator, &node.nodes);
 
         flatten_declaration(file->declarations[i], &node.nodes);
+
+        dynamic_array_append(&nodes_to_name_resolve, node);
     }
+
+    // for (u64 i = 0; i < nodes_to_name_resolve.count; i++) {
+    // }
 
     assert(file);
 }
@@ -130,7 +141,26 @@ void flatten_declaration(AST_Declaration *decl, Dynamic_Array<Flat_Node> *dest)
             break;
         }
 
-        case AST_Declaration_Kind::FUNCTION: assert(false);
+        case AST_Declaration_Kind::FUNCTION: {
+
+            for (u64 i = 0; i < decl->function.params.count; i++) {
+                flatten_type_spec(decl->function.params[i].type_spec, dest);
+                Flat_Node param_node = to_flat_node(decl->function.params[i]);
+                dynamic_array_append(dest, param_node);
+            }
+
+            if (decl->function.return_ts) {
+                flatten_type_spec(decl->function.return_ts, dest);
+            }
+
+            // At this point we should emit some kind of function header/prototype node
+
+            for (u64 i = 0; i < decl->function.body.count; i++) {
+                flatten_statement(decl->function.body[i], dest);
+            }
+
+            break;
+        }
 
         case AST_Declaration_Kind::STRUCT: assert(false);
         case AST_Declaration_Kind::UNION: assert(false);
@@ -146,15 +176,49 @@ void flatten_statement(AST_Statement *stmt, Dynamic_Array<Flat_Node> *dest)
 
     switch (stmt->kind) {
         case AST_Statement_Kind::INVALID: assert(false);
-        case AST_Statement_Kind::BLOCK: assert(false);
-        case AST_Statement_Kind::DECLARATION: assert(false);
-        case AST_Statement_Kind::ASSIGN: assert(false);
-        case AST_Statement_Kind::CALL: assert(false);
+
+        case AST_Statement_Kind::BLOCK: {
+
+            for (u64 i = 0; i < stmt->block.statements.count; i++) {
+                flatten_statement(stmt->block.statements[i], dest);
+            }
+            break;
+        }
+
+        case AST_Statement_Kind::DECLARATION: {
+            flatten_declaration(stmt->decl.decl, dest);
+            break;
+        }
+
+        case AST_Statement_Kind::ASSIGN: {
+            flatten_expression(stmt->assign.dest, dest);
+            flatten_expression(stmt->assign.value, dest);
+            break;
+        }
+
+        case AST_Statement_Kind::CALL: {
+            flatten_expression(stmt->call.call, dest);
+            break;
+        }
+
         case AST_Statement_Kind::IF: assert(false);
         case AST_Statement_Kind::WHILE: assert(false);
-        case AST_Statement_Kind::RETURN: assert(false);
-        case AST_Statement_Kind::PRINT: assert(false);
+
+        case AST_Statement_Kind::RETURN: {
+            if (stmt->return_stmt.value) {
+                flatten_expression(stmt->return_stmt.value, dest);
+            }
+            break;
+        }
+
+        case AST_Statement_Kind::PRINT: {
+            flatten_expression(stmt->print_expr, dest);
+            break;
+        }
     }
+
+    Flat_Node flat_stmt = to_flat_node(stmt);
+    dynamic_array_append(dest, flat_stmt);
 }
 
 void flatten_expression(AST_Expression *expr, Dynamic_Array<Flat_Node> *dest)
@@ -174,9 +238,23 @@ void flatten_expression(AST_Expression *expr, Dynamic_Array<Flat_Node> *dest)
 
         case AST_Expression_Kind::MEMBER: assert(false);
         case AST_Expression_Kind::INDEX: assert(false);
-        case AST_Expression_Kind::CALL: assert(false);
+
+        case AST_Expression_Kind::CALL: {
+            flatten_expression(expr->call.base, dest);
+
+            for (u64 i = 0 ; i < expr->call.args.count; i++) {
+                flatten_expression(expr->call.args[i], dest);
+            }
+            break;
+        }
+
         case AST_Expression_Kind::UNARY: assert(false);
-        case AST_Expression_Kind::BINARY: assert(false);
+
+        case AST_Expression_Kind::BINARY: {
+            flatten_expression(expr->binary.lhs, dest);
+            flatten_expression(expr->binary.rhs, dest);
+            break;
+        }
     }
 
     Flat_Node flat_expr = to_flat_node(expr);
@@ -189,14 +267,25 @@ void flatten_type_spec(AST_Type_Spec *ts, Dynamic_Array<Flat_Node> *dest)
 
     switch (ts->kind) {
         case AST_Type_Spec_Kind::INVALID: assert(false);
-        case AST_Type_Spec_Kind::NAME: assert(false);
-        case AST_Type_Spec_Kind::POINTER: assert(false);
+
+        case AST_Type_Spec_Kind::NAME: {
+            // Leaf
+            break;
+        }
+
+        case AST_Type_Spec_Kind::POINTER: {
+            flatten_type_spec(ts->base, dest);
+            break;
+        }
     }
+
+    Flat_Node flat_ts = to_flat_node(ts);
+    dynamic_array_append(dest, flat_ts);
 }
 
 Flat_Node to_flat_node(AST_Declaration *decl)
 {
-    Flat_Node result;
+    Flat_Node result = {};
     result.kind = Flat_Node_Kind::DECL;
     result.decl = decl;
     return result;
@@ -204,7 +293,7 @@ Flat_Node to_flat_node(AST_Declaration *decl)
 
 Flat_Node to_flat_node(AST_Statement *stmt)
 {
-    Flat_Node result;
+    Flat_Node result = {};
     result.kind = Flat_Node_Kind::STMT;
     result.stmt = stmt;
     return result;
@@ -212,7 +301,7 @@ Flat_Node to_flat_node(AST_Statement *stmt)
 
 Flat_Node to_flat_node(AST_Expression *expr)
 {
-    Flat_Node result;
+    Flat_Node result = {};
     result.kind = Flat_Node_Kind::EXPR;
     result.expr = expr;
     return result;
@@ -220,8 +309,16 @@ Flat_Node to_flat_node(AST_Expression *expr)
 
 Flat_Node to_flat_node(AST_Type_Spec *ts)
 {
-    Flat_Node result;
+    Flat_Node result = {};
     result.kind = Flat_Node_Kind::TS;
     result.ts = ts;
+    return result;
+}
+
+Flat_Node to_flat_node(const AST_Field_Declaration param)
+{
+    Flat_Node result = {};
+    result.kind = Flat_Node_Kind::TS;
+    result.param = param;
     return result;
 }
