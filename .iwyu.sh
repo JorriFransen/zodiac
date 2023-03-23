dc357974221a1b8868fb57e8b3bea778c883c11e
Jorri Fransen <jorri@jorrifransen.nl>
2023-03-23 16:18:52 +0100

    iwyu script ignores output with error in filenames, now greps for 'error:'.

--- .iwyu.sh
+++ .iwyu.sh
@@ -2,7 +2,7 @@
 
 if hash iwyu-tool
 then
-    iwyu-tool -o clang -p . -j $(nproc) -- -Xiwyu --verbose=1 | grep error
+    iwyu-tool -o clang -p . -j $(nproc) -- -Xiwyu --verbose=1 | grep 'error:'
 fi
 
 exit 0
