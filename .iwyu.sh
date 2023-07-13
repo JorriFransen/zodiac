5ce2cb0e99a1abcc1fcf3423a5b99e4c64ea1d72
Jorri Fransen <jorri@jorrifransen.nl>
2023-07-13 14:46:23 +0200

    Smarter iwyu detection.

--- .iwyu.sh
+++ .iwyu.sh
@@ -1,8 +1,17 @@
 #! /usr/bin/env bash
 
-if hash iwyu-tool
+IWYU_TOOL=""
+
+if hash iwyu-tool 2> /dev/null
 then
-    iwyu-tool -o clang -p . -j $(nproc) -- -Xiwyu --verbose=1 | grep 'error:'
+    IWYU_TOOL=iwyu-tool
 fi
+
+if hash iwyu_tool.py 2> /dev/null
+then
+    IWYU_TOOL=iwyu_tool.py
+fi
+
+$IWYU_TOOL -o clang -p . -j $(nproc) -- -Xiwyu --verbose=1 | grep 'error:'
 
 exit 0
