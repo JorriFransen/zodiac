5e41f4b59d062d077cded20a304298bccb04d727
Jorri Fransen <jorri@jorrifransen.nl>
2023-07-16 16:56:12 +0200

    Returning exit code from build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -7,7 +7,7 @@ echo "Building everyting..."
 make -f Makefile.linux.mak all -j $(nproc)
 ERR=$?
 if [ $ERR -ne 0 ]; then
-    echo "Error:"$ERR && exit
+    echo "Error:"$ERR && exit 1
 fi
 
 echo "All assemblies built successfully..."
