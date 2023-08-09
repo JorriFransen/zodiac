0d9b4c29bb1449e0a57e0a703496c1ca4ec6b8e4
Jorri Fransen <jorri@jorrifransen.nl>
2023-08-09 16:58:51 +0200

    Queue grow.

--- zodiac_lib/src/containers/queue.h
+++ zodiac_lib/src/containers/queue.h
@@ -50,6 +50,7 @@ s64 queue_empty(Queue<Element_Type> *queue)
 template <typename Element_Type>
 s64 queue_used(Queue<Element_Type> *queue)
 {
+    if (queue->front == -1 && queue->back == -1) return 0;
     if (queue->front == queue->back) return 1;
     else if (queue->front < queue->back) {
         debug_assert(queue->front >= 0 && queue->back > 0);
@@ -61,10 +62,36 @@ s64 queue_used(Queue<Element_Type> *queue)
 }
 
 template <typename Element_Type>
+void queue_grow(Queue<Element_Type> *queue)
+{
+    debug_assert(queue && queue->allocator && queue->capacity);
+    debug_assert(queue->front != -1);
+    debug_assert(queue->back != -1);
+    debug_assert(queue->front != queue->back);
+
+    auto new_cap = queue->capacity * 2;
+
+    Element_Type *new_data = alloc_array<Element_Type>(queue->allocator, new_cap);
+
+    if (queue->front < queue->back) {
+        zmemcpy(&new_data[queue->front], &queue->data[queue->front], ((queue->back + 1) - queue->front) * sizeof(Element_Type));
+    } else {
+        // TODO: Implement
+        assert_msg(false, "TODO: Implement!");
+    }
+    assert(new_data);
+
+    free(queue->allocator, queue->data);
+    queue->data = new_data;
+    queue->capacity = new_cap;
+
+}
+
+template <typename Element_Type>
 void queue_enqueue(Queue<Element_Type> *queue, Element_Type element)
 {
     if (queue_used(queue) == queue->capacity) {
-        assert_msg(false, "TODO: Grow queue");
+        queue_grow(queue);
     }
 
     if (queue->front == -1) {
