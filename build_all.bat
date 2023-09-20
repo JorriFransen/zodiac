4751c0cb04e6caea1c110361aa8cef2efa4e2147
Jorri Fransen <jorri@jorrifransen.nl>
2023-09-20 20:06:48 +0200

    Organizing make flags in build_all.bat.

--- build_all.bat
+++ build_all.bat
@@ -5,7 +5,11 @@ ECHO Building everything...
 
 for /f "tokens=*" %%f in ('wmic cpu get NumberOfLogicalProcessors /value ^| find "="') do set %%f
 
-make -j%NumberOfLogicalProcessors% -f Makefile.windows.mak all ZODIAC_VDL=
+SET VDL_FLAGS="ZODIAC_VDL=0"
+
+SET FLAGS=%VDL_FLAGS%
+
+make -j%NumberOfLogicalProcessors% -f Makefile.windows.mak all %FLAGS%
 IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
 ECHO All assemblies built successfully.
